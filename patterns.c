fe891ef86816ad3d2ca208e74739cf2ab0de1aa4
Sneha Esther Cherian <52592948+sneha1114@users.noreply.github.com>
2021-05-19 02:05:55 -0400

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -11,8 +11,8 @@ int main(void) {
   int m=1;
   
   if (pattern==1) {
-    for(int i=n; i >=1; i--) {
-     for(int j=1; j <=i; j++) {
+    for(int i=n;i>=1;i--) {
+     for(int j=1;j<=i;j++) {
       printf("#");
      }
      printf("\n");
